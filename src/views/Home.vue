9719009a1b9d993712dd263223b4482d4bf2248e
adam <adam.yh.dong@gmail.com>
2020-05-11 15:37:32 +0800

    新增首頁字幕特效

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,40 @@
   <div class="home">
     <img alt="Vue logo" src="@/assets/img/philosophy.jpg">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div class="title">{{myJson.homepage[0].title}}
-    </div>
+    <transition name="fadeTitle">
+      <div v-show="show" class="container title">
+        {{data.homepage[0].title}}
+      </div>
+    </transition>
+    <transition name="fadeTitle">
+      <div v-show="show" class="container content">
+        {{data.homepage[0].content}}
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import json from '@/assets/json/home.json'
+import data from '@/assets/json/home.json'
 
 export default {
   name: 'Home',
   data(){
     return{
-      myJson: json
+      data: data,
+      show: false
     }
   },
   components: {
     // HelloWorld
   },
-  mounted(){
-    console.log(this.myJson)
+  mounted() {
+    var self = this;
+    setInterval(function () {
+      self.show = true;
+    }, 100);
   }
 }
 </script>
@@ -41,8 +53,31 @@ export default {
   }
   .title{
     position: absolute;
-    left: 100px;
-    top: 100px;
+    left: 20px;
+    top: 135px;
+    font-size: 80px;
+    color:#fff;
+    width: 400px;
+    text-align: left;
+  }
+  .content{
+    position: absolute;
+    left: 20px;
+    top: 335px;
+    font-size: 30px;
+    color:#fff;
+    width: 400px;
+    text-align: left;
+  }
+  .container {
+    clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+  }
+  .fadeTitle-enter-active {
+    transition: all .5s ease-out;
+  }
+  .fadeTitle-enter {
+    clip-path: polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%);
+    transform: translate(0%, 100%);
   }
 }
 </style>
