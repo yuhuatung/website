8fa91fb38e044805579874224c79b2640a53f3f0
adam <adam.yh.dong@gmail.com>
2020-05-18 11:08:47 +0800

    新增 RED 規則

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -3,3 +3,15 @@
     <h1>This is a customers page</h1>
   </div>
 </template>
+
+<script>
+</script>
+<style lang="scss">
+@import "../assets/style/utils/_variables.scss";
+@media screen and (min-width: $bigWidth) {
+}
+@media screen and (min-width: $smallWidth) and (max-width: $bigWidth) {
+}
+@media screen and (max-width: $smallWidth) {
+}
+</style>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -3,3 +3,15 @@
     <h1>This is a services page</h1>
   </div>
 </template>
+
+<script>
+</script>
+<style lang="scss">
+@import "../assets/style/utils/_variables.scss";
+@media screen and (min-width: $bigWidth) {
+}
+@media screen and (min-width: $smallWidth) and (max-width: $bigWidth) {
+}
+@media screen and (max-width: $smallWidth) {
+}
+</style>
