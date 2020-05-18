c39bf18505b92c896e4299a821af1673e6e824a3
adam <adam.yh.dong@gmail.com>
2020-05-18 17:56:15 +0800

    新增顧客頁面

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="customers">
-    <h1>This is a customers page</h1>
+  <div class="customers-body">
+    <div class="top">
+      <div class="bigLogo">Success Case
+      </div>
+      <div class="smallLogo">Our Customers
+      </div>
+      <div class="customer-content">
+        Wanlian has been involved in many projects, utilizing an amalgamation of various solutions.
+      </div>
+    </div>
+    <div class="customers"></div>
   </div>
 </template>
 
@@ -13,5 +22,42 @@
 @media screen and (min-width: $smallWidth) and (max-width: $bigWidth) {
 }
 @media screen and (max-width: $smallWidth) {
+}
+.customers-body {
+  background-image:url("~@/assets/img/recruitment.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100%;
+  .top{
+    height: 750px;
+    color: white;
+    position: relative;
+    .bigLogo{
+      font-size: 96px;
+      position: absolute;
+      top: 120px;
+      left: 40px;
+    }
+    .smallLogo{
+      font-size: 64px;
+      position: absolute;
+      top: 240px;
+      left: 50px;
+    }
+    .customer-content{
+      font-size: 24px;
+      position: absolute;
+      bottom: 80px;
+      right: 50px;
+      width: 413px;
+      text-align: left;
+    }
+  }
+  .customers{
+    height: 750px;
+  }
+  .tooter{
+
+  }
 }
 </style>
