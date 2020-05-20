b244209b359cd684cec847267f2eb87831263e45
Youfuhe <40340135@gm.nfu.edu.tw>
2020-05-20 17:59:59 +0800

    顧客頁面 RWD開發

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -4,30 +4,40 @@
       <video class="sleep-video" autoplay muted loop>
         <source src="https://www.308262.com/vue/files/customers.mov" type="video/mp4">
       </video>
-      <div class="bigLogo">Success Case
-      </div>
-      <div class="smallLogo">Our Customers
-      </div>
-      <div class="customer-content">
-        Wanlian has been involved in many projects, utilizing an amalgamation of various solutions.
-      </div>
+      <img
+        v-show="screenwidth<810"
+        class="sleep-video"
+        src="@/assets/img/customer.jpg"
+        alt="customersImg"
+      />
+      <div class="bigLogo">Success Case</div>
+      <div class="smallLogo">Our Customers</div>
+      <div
+        class="customer-content"
+      >Wanlian has been involved in many projects, utilizing an amalgamation of various solutions.</div>
     </div>
-    <div class="customers" :style="{ height: customersHeight + 'px' }" ref="customers">
+    <div
+      v-show="true"
+      class="customers"
+      :style="{ height: customersHeight + 'px' }"
+      ref="customers"
+    >
       <img src="@/assets/img/icons8-expand_arrow-3_2x.png" class="arrow" @click="toCustomers(step)" />
       <div v-for="(item, index) in data.customers" :key="index" class="content">
         <div class="customers-list">
           <video class="sleep-video" v-if="item.type == 'video'" controls>
-            <source :src="item.url" type="video/mp4">
+            <source :src="item.url" type="video/mp4" />
           </video>
-          <img :src="item.url" v-if="item.type == 'image'"/>
+          <img :src="item.url" v-if="item.type == 'image'" />
           <div class="customers-content">
             <div class="content-cht">{{item.contentCht}}</div>
-            <div class="content-eng">{{item.contentEng}}</div>
+            <div v-show="screenwidth>810" class="content-eng">{{item.contentEng}}</div>
           </div>
+          <div v-show="screenwidth<810" class="content-eng">{{item.contentEng}}</div>
         </div>
       </div>
     </div>
-    <div class="footer">
+    <div class="footer" v-show="false">
       <div class="media">
         <img src="@/assets/img/icons8-facebook_new_2x.png" />
         <img src="@/assets/img/icons8-facebook_messenger_2x.png" />
@@ -41,18 +51,18 @@
 import data from "@/assets/json/customers.json";
 export default {
   name: "Customers",
+  props: ["screenwidth"],
   data() {
     return {
       data: data,
-      step: 50,
+      step: 50
     };
   },
-  components: {
-  },
+  components: {},
   computed: {
     customersHeight() {
       let numOfData = data.customers.length;
-      return numOfData*320 + 80;
+      return numOfData * 320 + 80;
     }
   },
   methods: {
@@ -65,10 +75,9 @@ export default {
       } else {
         clearTimeout(c);
       }
-    },
+    }
   },
-  mounted() {
-  },
+  mounted() {},
   beforeDestroy() {
     document.documentElement.scrollTop = 0;
   }
@@ -77,95 +86,194 @@ export default {
 <style lang="scss">
 @import "../assets/style/utils/_variables.scss";
 @media screen and (min-width: $bigWidth) {
-}
-@media screen and (min-width: $smallWidth) and (max-width: $bigWidth) {
-}
-@media screen and (max-width: $smallWidth) {
-}
-.customers-body {
-  .top{
-    height: 750px;
-    color: white;
-    position: relative;
+  .top {
+    height: 90vh;
     .sleep-video {
-      left: 0;
-      top: 0;
-      position: fixed;
-      // width: 100%;
       min-height: 100vh;
       min-width: 100vw;
-      z-index: -20;
+      top: 40%;
+      transform: translateX(-50%) translateY(-50%);
+      left: 50%;
     }
-    .bigLogo{
+    .bigLogo {
       font-size: 96px;
-      position: absolute;
       top: 120px;
       left: 40px;
     }
-    .smallLogo{
+    .smallLogo {
       font-size: 64px;
-      position: absolute;
       top: 240px;
       left: 50px;
     }
-    .customer-content{
-      font-size: 24px;
-      position: absolute;
-      bottom: 80px;
-      right: 50px;
+    .customer-content {
       width: 413px;
+      font-size: 24px;
+      bottom: 70px;
+      right: 50px;
+    }
+  }
+  .customers {
+    margin-top: 90vh;
+    .customers-list{
+      .customers-content {
+        width: 700px;
+      }
+    }
+  }
+}
+@media screen and (min-width: $smallWidth) and (max-width: $bigWidth) {
+  .top {
+    height: 45vh;
+    min-height: 400px;
+    .sleep-video {
+      height: 45vh;
+      min-height: 400px;
+      width: 100%;
+      top: 0;
+      left: 0;
+    }
+    .bigLogo {
+      font-size: 55px;
+      top: 80px;
+      left: 30px;
+    }
+    .smallLogo {
+      font-size: 26px;
+      top: 150px;
+      left: 30px;
+    }
+    .customer-content {
+      width: 180px;
+      font-size: 16px;
+      top: 270px;
+      right: 50px;
+    }
+  }
+  .customers {
+    margin-top: 400px;
+    .customers-list{
+      flex-wrap: wrap;
+      .customers-content {
+        width: 40%;
+      }
+    }
+  }
+}
+@media screen and (max-width: $smallWidth) {
+  .top {
+    height: 85vh;
+    max-height: 650px;
+    min-height: 550px;
+    background-color: #000;
+    width: 100%;
+
+    .sleep-video {
+      height: 240px;
+      width: 100%;
+      top: 28%;
+      left: 0;
+    }
+    .bigLogo {
+      font-size: 45px;
+      top: 16%;
+      left: 5%;
+    }
+    .smallLogo {
+      font-size: 26px;
+      top: 23%;
+      left: 5%;
+    }
+    .customer-content {
+      width: 180px;
+      font-size: 16px;
+      bottom: 50px;
+      right: 5%;
+    }
+  }
+  .customers {
+    margin-top: 85vh;
+  }
+}
+.customers-body {
+  .top {
+    color: white;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: -1;
+    .sleep-video {
+      position: absolute;
+    }
+    .bigLogo {
+      position: absolute;
+    }
+    .smallLogo {
+      position: absolute;
+    }
+    .customer-content {
+      position: absolute;
       text-align: left;
     }
   }
-  .customers{
+  .customers {
     border-top-left-radius: 20px;
     border-top-right-radius: 20px;
     background-color: #fff;
-    .arrow{
+
+    .arrow {
       margin-top: 15px;
       width: 32px;
       cursor: pointer;
     }
-    .customers-list{
-      width: 1064px;
-      height: 280px;
+    .customers-list {
+      // width: 1064px;
+      // height: 280px;
       margin: 20px auto;
+      padding: 0 30px;
       display: flex;
-      video, img{
+      
+      justify-content: center;
+      align-items: center;
+      video,
+      img {
         width: 353px;
         height: 265px;
+        min-width: 353px;
       }
-      .customers-content{
-        width: 700px;
+      .customers-content {
         height: 265px;
         text-align: left;
         padding-top: 10px;
         padding-left: 30px;
-        position: relative;
-        .content-cht{
+        // position: relative;
+        display: flex;
+        flex-direction: column;
+
+        .content-cht {
           font-size: 16px;
         }
-        .content-eng{
-          top: 180px;
-          position: absolute;
+        .content-eng {
+          margin-top: auto;
+          margin-bottom: 30px;
           font-size: 12px;
           color: rgb(145, 145, 145);
         }
       }
     }
   }
-  .footer{
+  .footer {
     height: 142px;
     background-color: rgb(94, 94, 94);
     display: flex;
-    .media{
+    .media {
       margin-top: 45px;
       margin-left: 145px;
-      img{
+      img {
         width: 40px;
         margin-right: 30px;
       }
-    }.company{
+    }
+    .company {
       margin-top: 56px;
       color: white;
       font-size: 16px;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: '/frontend/',
   css: {
     loaderOptions: {
       less: {
